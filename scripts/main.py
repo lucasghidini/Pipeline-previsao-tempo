07f19e1c190a9242811de5050d1b3f7a2d7ff835
Lucas Ghidini <lucasdemattos1306@gmail.com>
2025-08-15 20:53:45 -0300

    feat: carregando a chave api no scrpit main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,21 @@
 import requests
 import csv
 from datetime import date, timedelta, datetime
+from dotenv import load_dotenv
+import os
+
+# carregando a chave da api
+load_dotenv()
+
+API_KEY = os.getenv('OPENWEATHER_API_KEY')
+if not API_KEY:
+    raise ValueError('A chave da api "OPENWEATHER_API_KEY" não foi encontrada. Verifique o arquivo')
+
+
+
+# Criando classe para iniciar a pipeline
+class WeatherPipeline:
+    """
+    Uma classe para orquestrar um pipeline de extração e transformação dos dados
+    de clima da API oficial OpenWeather
+    """
