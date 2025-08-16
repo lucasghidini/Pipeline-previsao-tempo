563bbbee03e4e5f9f55101b7734e0a88d093bec9
Lucas Ghidini <lucasdemattos1306@gmail.com>
2025-08-15 21:06:26 -0300

    feat: add método de extração da previsão atual

--- scripts/main.py
+++ scripts/main.py
@@ -19,7 +19,8 @@ class WeatherPipeline:
     Uma classe para orquestrar um pipeline de extração e transformação dos dados
     de clima da API oficial OpenWeather
     """
-    base_url = 'https://api.openweathermap.org/data/2.5/forecast'
+    base_url_atual = 'https://api.openweathermap.org/data/2.5/weather'
+    base_url_previsao = 'https://api.openweathermap.org/data/2.5/forecast'
 
     def __init__(self, api_key, city):
         self.api_key = api_key
@@ -27,4 +28,23 @@ class WeatherPipeline:
         self.raw_data = None
         self.processed_data = None
     
-  
+    def extract_prev_atual(self):
+        """
+        Método para a extrção dos dados da previsão atual da API,
+        Guarda a lista bruta no atributo self.raw_data
+        """
+        print(f'Extraindo dados para {self.city}')
+        params = {
+            'q':self.city,
+            'appid': self.api_key,
+            'units': 'metric',
+            'lang':'pt_bt'
+        }
+        try:
+            response = requests.get(self.base_url_atual, params= params)
+            response.raise_for_status()
+            self.raw_data = response.json().get('list',[])
+            print('Extração concluida !')
+        except requests.exceptions.RequestException as e:
+            print(f'Erro de extração: {e}')
+            self.raw_data = []
