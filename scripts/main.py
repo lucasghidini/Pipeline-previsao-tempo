de0775845ebfc20c7c9a4a0c58dcd640c31eae8d
Lucas Ghidini <lucasdemattos1306@gmail.com>
2025-08-15 21:55:30 -0300

    feat: add main file

--- scripts/main.py
+++ scripts/main.py
@@ -1,114 +1,32 @@
-import requests
-import csv
-from datetime import date, timedelta, datetime
+from pipeline import WeatherPipeline
 from dotenv import load_dotenv
 import os
 
-# carregando a chave da api
-load_dotenv()
+if __name__ == '__main__':
+    # carregando a chave da api
+    load_dotenv()
 
-API_KEY = os.getenv('OPENWEATHER_API_KEY')
-if not API_KEY:
-    raise ValueError('A chave da api "OPENWEATHER_API_KEY" não foi encontrada. Verifique o arquivo')
+    API_KEY = os.getenv('OPENWEATHER_API_KEY')
+    if not API_KEY:
+        raise ValueError('A chave da api "OPENWEATHER_API_KEY" não foi encontrada. Verifique o arquivo')
 
+    city = input('Digite o nome da cidade que deseja buscar os dados, certivique que esetaja escrito corretamente e que a cidade exista !')
 
+    pipeline = WeatherPipeline(api_key=API_KEY, city= city)
 
-# Criando classe para iniciar a pipeline
-class WeatherPipeline:
-    """
-    Uma classe para orquestrar um pipeline de extração e transformação dos dados
-    de clima da API oficial OpenWeather
-    """
-    base_url_atual = 'https://api.openweathermap.org/data/2.5/weather'
-    base_url_previsao = 'https://api.openweathermap.org/data/2.5/forecast'
+    pipeline.run()
 
-    def __init__(self, api_key, city):
-        self.api_key = api_key
-        self.city = city
-        self.current_data = None
-        self.raw_data = None
-        self.processed_data = None
+    print('RESULTADO DO FINAL DO PIPELINE')
+    if pipeline.current_data:
+        print('[Clima Atual]')
+        print(f'Temperatura: {pipeline.current_data.get('temperatura_atual')}ºC')
+        print(f'Sensação Termica: {pipeline.current_data.get('sensacao_termica')}ºc')
+        print(f'Clima: {pipeline.current_data.get('clima')}')
     
-    def extract_prev_atual(self):
-        """
-        Método para a extrção dos dados da previsão atual da API,
-        Guarda a lista bruta no atributo self.raw_data
-        """
-        print(f'Extraindo dados para {self.city}')
-        params = {
-            'q':self.city,
-            'appid': self.api_key,
-            'units': 'metric',
-            'lang':'pt_bt'
-        }
-        try:
-            response = requests.get(self.base_url_atual, params= params)
-            response.raise_for_status()
-            data = response.json()
+    if pipeline.processed_data:
+        print('[Previsão para os proximos dias]')
+        for previsao in pipeline.processed_data:
+            print(f'- {previsao['data']}: Temp. Max. {previsao['temp_max']}ºC, {previsao['clima']}')
 
-            main_data = data.get('main',{})
-            self.current_data = {
-                'Cidade': data.get('name'),
-                'Temperatura Atual': main_data.get('temp'),
-                'Sensação Termica': main_data.get('feels_like'),
-                'Clima': data.get('weather', [{}])[0].get('description')
-            }
-            print('Extração concluida !')
-        except requests.exceptions.RequestException as e:
-            print(f'Erro de extração: {e}')
-            self.current_data = {}
+    print('---Pipeline finalizado---')
     
-    def extract_prev(self):
-        """
-        Metodo para extrair os dados da previsão futura da API
-        """
-        print(f'Extraidno a previsão futura para {self.city}')
-        params = {
-            'q':self.city,
-            'appid': self.api_key,
-            'units': 'metric',
-            'lang':'pt_bt'
-        }
-        try:
-            response = requests.get(self.base_url_previsao, params= params)
-            response.raise_for_status()
-            self.raw_data = response.json().get('list', [])
-            print('Extração da previsão concluido !')
-        except requests.exceptions.RequestException as e:
-            print(f'Erro ao extrair: {e}')
-            self.raw_data =[]
-
-    def transform_prev(self):
-        """
-        Método para transformar os dados brutos da previsão
-        """
-        if not self.raw_data:
-            print('Transformação pulada: Não a dados de previsão para procesar')
-            self.raw_data = []
-            return
-
-        print('Trasformando dados da previsão futura...')
-        hoje = date.today()
-        limite_data = hoje + timedelta(days=4)
-        previsoes_diarias = {}
-
-        for previsao in self.raw_data:
-            timestamp = datetime.strptime(previsao['dt_txt'],'%Y-%m-%d %H:%M:%S')
-            data_previsao = timestamp.date()
-
-            if hoje < data_previsao < limite_data:
-                temp_atual = previsao['main']['temp']
-
-                if data_previsao not in previsoes_diarias:
-                    previsoes_diarias[data_previsao] = {
-                        'data': data_previsao.strftime('%Y-%m-%d'),
-                        'temp_max':temp_atual,
-                        'clima_representativo': previsao['weather'][0]['description']
-                    }
-                else:
-                    if temp_atual > previsoes_diarias[data_previsao]['temp_max']:
-                        previsoes_diarias[data_previsao]['temp_max'] = temp_atual
-                        previsoes_diarias[data_previsao]['clima_representativo'] = previsao['weather'][0]['description']
-        
-        self.processed_data = list(previsoes_diarias.values())
-        print(f'Transformação concluída ! {len(self.processed_data)} dias processados.')
---
+++ scripts/pipeline.py
@@ -0,0 +1,114 @@
+import requests
+import csv
+from datetime import date, timedelta, datetime
+
+
+
+
+# Criando classe para iniciar a pipeline
+class WeatherPipeline:
+    """
+    Uma classe para orquestrar um pipeline de extração e transformação dos dados
+    de clima da API oficial OpenWeather
+    """
+    base_url_atual = 'https://api.openweathermap.org/data/2.5/weather'
+    base_url_previsao = 'https://api.openweathermap.org/data/2.5/forecast'
+
+    def __init__(self, api_key, city):
+        self.api_key = api_key
+        self.city = city
+        self.current_data = None
+        self.raw_data = None
+        self.processed_data = None
+    
+    def extract_prev_atual(self):
+        """
+        Método para a extrção dos dados da previsão atual da API,
+        Guarda a lista bruta no atributo self.raw_data
+        """
+        print(f'Extraindo dados para {self.city}')
+        params = {
+            'q':self.city,
+            'appid': self.api_key,
+            'units': 'metric',
+            'lang':'pt_br'
+        }
+        try:
+            response = requests.get(self.base_url_atual, params= params)
+            response.raise_for_status()
+            data = response.json()
+
+            main_data = data.get('main',{})
+            self.current_data = {
+                'cidade': data.get('name'),
+                'temperatura Atual': main_data.get('temp'),
+                'sensação Termica': main_data.get('feels_like'),
+                'clima': data.get('weather', [{}])[0].get('description')
+            }
+            print('Extração concluida !')
+        except requests.exceptions.RequestException as e:
+            print(f'Erro de extração: {e}')
+            self.current_data = {}
+    
+    def extract_prev(self):
+        """
+        Metodo para extrair os dados da previsão futura da API
+        """
+        print(f'Extraidno a previsão futura para {self.city}')
+        params = {
+            'q':self.city,
+            'appid': self.api_key,
+            'units': 'metric',
+            'lang':'pt_br'
+        }
+        try:
+            response = requests.get(self.base_url_previsao, params= params)
+            response.raise_for_status()
+            self.raw_data = response.json().get('list', [])
+            print('Extração da previsão concluido !')
+        except requests.exceptions.RequestException as e:
+            print(f'Erro ao extrair: {e}')
+            self.raw_data =[]
+
+    def transform_prev(self):
+        """
+        Método para transformar os dados brutos da previsão
+        """
+        if not self.raw_data:
+            print('Transformação pulada: Não a dados de previsão para procesar')
+            self.raw_data = []
+            return
+
+        print('Trasformando dados da previsão futura...')
+        hoje = date.today()
+        limite_data = hoje + timedelta(days=4)
+        previsoes_diarias = {}
+
+        for previsao in self.raw_data:
+            timestamp = datetime.strptime(previsao['dt_txt'],'%Y-%m-%d %H:%M:%S')
+            data_previsao = timestamp.date()
+
+            if hoje < data_previsao < limite_data:
+                temp_atual = previsao.get(['main']['temp'])
+
+                if data_previsao not in previsoes_diarias:
+                    previsoes_diarias[data_previsao] = {
+                        'data': data_previsao.strftime('%Y-%m-%d'),
+                        'temp_max':temp_atual,
+                        'clima_representativo': previsao['weather'][0]['description']
+                    }
+                else:
+                    if temp_atual > previsoes_diarias[data_previsao]['temp_max']:
+                        previsoes_diarias[data_previsao]['temp_max'] = temp_atual
+                        previsoes_diarias[data_previsao]['clima_representativo'] = previsao['weather'][0]['description']
+        
+        self.processed_data = list(previsoes_diarias.values())
+        print(f'Transformação concluída ! {len(self.processed_data)} dias processados.')
+    
+    def run(self):
+        """
+        Executa todas as pipelines
+        """
+        self.extract_prev_atual()
+        self.extract_prev()
+        self.transform_prev()
