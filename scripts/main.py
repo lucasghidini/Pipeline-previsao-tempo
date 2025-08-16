35300d2ac800370329dc36039a11d1a59cf06357
Lucas Ghidini <lucasdemattos1306@gmail.com>
2025-08-15 22:33:43 -0300

    feat: add função para salvar os dados em um arquivo .json na pasta dados_salvos

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,25 @@
 from pipeline import WeatherPipeline
 from dotenv import load_dotenv
 import os
+import json
+
+def salvar(dados, nome_pasta, nome_arquivo):
+    """
+    Salva os dados em um arquivo .json dentro da pasta dados_salvos
+    """
+    if not nome_arquivo.endwith('.json'):
+        print(f'Aviso: O nome do arquivo "{nome_arquivo}" foi alterado para terminar com .json')
+        nome_arquivo += '.json'
+
+    os.makedirs(nome_pasta, exist_ok=True)
+
+    caminho_completo = os.path.join(nome_pasta,nome_arquivo)
+    try:
+        with open(caminho_completo, 'w', encoding= 'utf-8') as f:
+            json.dump(dados,f, indent=4, ensure_ascii= False)
+        print('Dados salvos !')
+    except IOError as e:
+        print(f'Erro: {e}')
 
 if __name__ == '__main__':
     # carregando a chave da api
@@ -10,7 +29,7 @@ if __name__ == '__main__':
     if not API_KEY:
         raise ValueError('A chave da api "OPENWEATHER_API_KEY" não foi encontrada. Verifique o arquivo')
 
-    city = 'São Paulo' #input('Digite o nome da cidade que deseja buscar os dados, certivique que esetaja escrito corretamente e que a cidade exista !')
+    city = 'Santos,BR' #input('Digite o nome da cidade que deseja buscar os dados, certivique que esetaja escrito corretamente e que a cidade exista !')
 
     pipeline = WeatherPipeline(api_key=API_KEY, city= city)
 
